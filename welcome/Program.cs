5c01ffdc4f2c8db31c1238fc858aed689317ca81
pashwiniis <pashwini684@gmail.com>
2021-11-13 23:52:55 +0530

    added new one

---
+++ welcome/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Helloworld
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
